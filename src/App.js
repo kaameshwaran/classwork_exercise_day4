7bbe8a6e1fb62e70543dd844e3a2d37e59fa6739
kaamehwaran <727721euec062@skcet.ac.in>
2023-03-29 22:21:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FruitSurvey from './components/fruitSurvey';
+
+function App() {
+  return (
+    <div className="App">
+      <FruitSurvey /> 
+    </div>
+  );
+}
+
+export default App;
